ead130f6dd0bc31abb8747ba3e32135087843048
ThinkingDobby <chiho1221@naver.com>
2021-09-09 20:57:52 +0900

    TrackerActivity 코드 정리
    <FIN>
    변수, 주석 정리
    cameraView 크기 조정
    메뉴 버튼 동작 추가

--- app/src/main/java/com/example/thinkingdobby/fishtracker/TrackerActivity.kt
+++ app/src/main/java/com/example/thinkingdobby/fishtracker/TrackerActivity.kt
@@ -2,19 +2,14 @@ package com.example.thinkingdobby.fishtracker
 
 import android.Manifest
 import android.app.Activity
-import android.content.Context
 import android.content.Intent
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.graphics.Color
 import android.graphics.Paint
-import android.media.AudioManager
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.View
-import android.widget.Button
-import android.widget.ImageView
-import android.widget.TextView
 import com.karumi.dexter.Dexter
 import com.karumi.dexter.MultiplePermissionsReport
 import com.karumi.dexter.PermissionToken
@@ -33,24 +28,12 @@ class TrackerActivity : AppCompatActivity(), View.OnClickListener {
     private var classifier: Classifier? = null
     private val executor: Executor = Executors.newSingleThreadExecutor()
     private var cameraView: CameraView? = null
-    private var tracker_tv_result: TextView? = null
-    private var tracker_tv_resultSub: TextView? = null
-    private var tracker_iv_selected: ImageView? = null
-    private var tracker_btn_shot: Button? = null
-    private var tracker_btn_album: Button? = null
-    private var tracker_tv_album: TextView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_tracker)
-        cameraView = findViewById<View>(R.id.cameraView) as CameraView
-        tracker_tv_result = findViewById<View>(R.id.tracker_tv_result) as TextView
-        tracker_tv_resultSub = findViewById<View>(R.id.tracker_tv_resultSub) as TextView
-        tracker_iv_selected = findViewById<View>(R.id.tracker_iv_selected) as ImageView
-        tracker_btn_album = findViewById<View>(R.id.tracker_btn_album) as Button
-        tracker_tv_album = findViewById<View>(R.id.tracker_tv_album) as TextView
-        tracker_btn_shot = findViewById<View>(R.id.tracker_btn_shot) as Button
 
+        cameraView = findViewById<View>(R.id.cameraView) as CameraView
         cameraView!!.start()
 
         initVisibility()
@@ -58,6 +41,7 @@ class TrackerActivity : AppCompatActivity(), View.OnClickListener {
 
         // scale, seekBar 이용해 크기 조절 구현 시도할 것
 
+        // 상태바 투명화
         window.apply {
             decorView.systemUiVisibility =
                     View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
@@ -65,13 +49,17 @@ class TrackerActivity : AppCompatActivity(), View.OnClickListener {
             statusBarColor = Color.TRANSPARENT
         }
 
-        // btn events delegation
-        tracker_btn_album!!.setOnClickListener(this)
-        tracker_tv_album!!.setOnClickListener(this)
-        tracker_btn_shot!!.setOnClickListener(this)
+        tracker_btn_album.setOnClickListener(this)
+        tracker_tv_album.setOnClickListener(this)
+        tracker_btn_shot.setOnClickListener(this)
+        tracker_iv_menu.setOnClickListener {
+            val intent = Intent(this@TrackerActivity, MenuActivity::class.java)
+            startActivity(intent)
+        }
 
         // initialize tensorflow async
         initTensorFlowAndLoadModel()
+
         // permission check & request if needed
         Dexter.withActivity(this)
                 .withPermissions(
@@ -85,8 +73,10 @@ class TrackerActivity : AppCompatActivity(), View.OnClickListener {
                     override fun onPermissionRationaleShouldBeShown(permissions: List<PermissionRequest>, token: PermissionToken) { /* ... */
                     }
                 }).check()
+
         // cameraview library has its own permission check method
         cameraView!!.setPermissions(CameraKit.Constants.PERMISSIONS_PICTURE)
+
         // invoke tensorflow inference when picture taken from camera
         cameraView!!.setCameraListener(object : CameraListener() {
             override fun onPictureTaken(picture: ByteArray) {
@@ -100,6 +90,16 @@ class TrackerActivity : AppCompatActivity(), View.OnClickListener {
     override fun onDestroy() {
         super.onDestroy()
         executor.execute { classifier!!.close() }
+    }
+
+    override fun onClick(v: View) {
+        val id = v.id
+        when (id) {
+            R.id.tracker_btn_album, R.id.tracker_tv_album -> loadImageFromGallery()
+            R.id.tracker_btn_shot -> cameraView!!.captureImage()
+            else -> {
+            }
+        }
     }
 
     private fun initTensorFlowAndLoadModel() {
@@ -120,59 +120,17 @@ class TrackerActivity : AppCompatActivity(), View.OnClickListener {
         }
     }
 
-    override fun onClick(v: View) { // define which methods to call when buttons in view clicked
-        val id = v.id
-        when (id) {
-            R.id.tracker_btn_album, R.id.tracker_tv_album -> LoadImageFromGallery()
-            R.id.tracker_btn_shot -> {
-                cameraView!!.captureImage()
-            }
-            else -> {
-            }
-        }
-    }
-
-    // recognize image from camera roll.
-    private fun LoadImageFromGallery() {
-        // invoke image picker to get a single image to be inferenced
+    private fun loadImageFromGallery() {
         ImageSelectorActivity.start(this@TrackerActivity, 1, ImageSelectorActivity.MODE_SINGLE, false, false, false)
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent) { // pass the selected image from image picker to tensorflow
-// image picker returns image(s) in arrayList
+        // image picker returns image(s) in arrayList
         if (resultCode == Activity.RESULT_OK && requestCode == ImageSelectorActivity.REQUEST_IMAGE) {
             val images = data.getSerializableExtra(ImageSelectorActivity.REQUEST_OUTPUT) as ArrayList<String>
             // image decoded to bitmap, which can be recognized by tensorflow
             val bitmap = BitmapFactory.decodeFile(images[0])
             recognize_bitmap(bitmap)
-        }
-    }
-
-    // retrieves image from entered url string and call tensorflow
-
-    // recognize bitmap and get results
-    private fun recognize_bitmap(bitmap: Bitmap) { // create a bitmap scaled to INPUT_SIZE
-        var bitmap = bitmap
-        bitmap = Bitmap.createScaledBitmap(bitmap, INPUT_SIZE, INPUT_SIZE, false)
-        // returned value stores in Classifier.Recognition format
-// which provides various methods to parse the result,
-// but I'm going to show raw result here.
-        val results = classifier!!.recognizeImage(bitmap)
-        val finalBitmap = bitmap
-        runOnUiThread {
-            tracker_iv_selected!!.setImageBitmap(finalBitmap)
-            setVisibility()
-            tracker_tv_resultProbability.text = "${probPicker(results[0].toString())}% 확률로"
-            tracker_tv_result!!.text = nameEngToKor(namePicker(results[0].toString()))
-            if (results.size > 1) {
-                tracker_tv_resultSub!!.text =
-                        "또는 ${probPicker(results[1].toString())}% 확률로 ${nameEngToKor(namePicker(results[1].toString()))} 입니다."
-                if (results.size > 2) tracker_tv_resultSubEnd.visibility = View.VISIBLE
-                else tracker_tv_resultSubEnd.visibility = View.INVISIBLE
-            } else {
-                tracker_tv_resultSub!!.text = ""
-                tracker_tv_resultSubEnd.visibility = View.INVISIBLE
-            }
         }
     }
 
@@ -187,6 +145,29 @@ class TrackerActivity : AppCompatActivity(), View.OnClickListener {
         tracker_iv_selectedShadow.visibility = View.VISIBLE
         tracker_tv_guide.visibility = View.INVISIBLE
         tracker_tv_resultEnd.visibility = View.VISIBLE
+    }
+
+    // recognize bitmap and get results
+    private fun recognize_bitmap(bitmap: Bitmap) { // create a bitmap scaled to INPUT_SIZE
+        var bitmap = bitmap
+        bitmap = Bitmap.createScaledBitmap(bitmap, INPUT_SIZE, INPUT_SIZE, false)
+        val results = classifier!!.recognizeImage(bitmap)
+        val finalBitmap = bitmap
+        runOnUiThread {
+            tracker_iv_selected.setImageBitmap(finalBitmap)
+            setVisibility()
+            tracker_tv_resultProbability.text = "${probPicker(results[0].toString())}% 확률로"
+            tracker_tv_result.text = nameEngToKor(namePicker(results[0].toString()))
+            if (results.size > 1) {
+                tracker_tv_resultSub.text =
+                        "또는 ${probPicker(results[1].toString())}% 확률로 ${nameEngToKor(namePicker(results[1].toString()))} 입니다."
+                if (results.size > 2) tracker_tv_resultSubEnd.visibility = View.VISIBLE
+                else tracker_tv_resultSubEnd.visibility = View.INVISIBLE
+            } else {
+                tracker_tv_resultSub.text = ""
+                tracker_tv_resultSubEnd.visibility = View.INVISIBLE
+            }
+        }
     }
 
     private fun probPicker(input: String): String {
