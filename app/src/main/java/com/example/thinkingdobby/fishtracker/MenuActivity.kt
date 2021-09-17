9ac7776214d986d12b220ace5772ab64342024d8
ThinkingDobby <chiho1221@naver.com>
2021-09-17 22:40:30 +0900

    MenuActivity 디자인 적용중
    <FIN>
    디자인 적용중
    <TODO>
    카드뷰 테두리 처리 - 또는 다른 방안 탐색
    LinearLayout 텍스트 잘리는 문제 해결 - height 늘려볼 것

--- app/src/main/java/com/example/thinkingdobby/fishtracker/MenuActivity.kt
+++ app/src/main/java/com/example/thinkingdobby/fishtracker/MenuActivity.kt
@@ -2,11 +2,14 @@ package com.example.thinkingdobby.fishtracker
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MenuActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu)
+
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
     }
 }
