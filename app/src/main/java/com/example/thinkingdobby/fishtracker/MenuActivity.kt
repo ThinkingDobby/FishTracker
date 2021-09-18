0dd9c131e3967d116cc80997c990ace2153241fa
ThinkingDobby <chiho1221@naver.com>
2021-09-18 20:59:50 +0900

    MenuActivity 디자인 임시 적용 완료

--- app/src/main/java/com/example/thinkingdobby/fishtracker/MenuActivity.kt
+++ app/src/main/java/com/example/thinkingdobby/fishtracker/MenuActivity.kt
@@ -3,6 +3,7 @@ package com.example.thinkingdobby.fishtracker
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import kotlinx.android.synthetic.main.activity_menu.*
 
 class MenuActivity : AppCompatActivity() {
 
@@ -11,5 +12,9 @@ class MenuActivity : AppCompatActivity() {
         setContentView(R.layout.activity_menu)
 
         window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+
+        menu_btn_back.setOnClickListener {
+            finish()
+        }
     }
 }
