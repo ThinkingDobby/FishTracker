02a163c8aab138559a5afb1a2bda08270523e25c
ThinkingDobby <chiho1221@naver.com>
2021-10-28 19:56:47 +0900

    CollectionActivity 제작 전 임시 저장

--- app/src/main/java/com/example/thinkingdobby/fishtracker/MenuActivity.kt
+++ app/src/main/java/com/example/thinkingdobby/fishtracker/MenuActivity.kt
@@ -1,5 +1,6 @@
 package com.example.thinkingdobby.fishtracker
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -12,6 +13,11 @@ class MenuActivity : AppCompatActivity() {
         setContentView(R.layout.activity_menu)
 
         window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+
+        menu_cv_1.setOnClickListener {
+            val intent = Intent(this, CollectionActivity::class.java)
+            startActivity(intent)
+        }
 
         menu_btn_back.setOnClickListener {
             finish()
