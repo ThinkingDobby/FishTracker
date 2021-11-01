cb657f5f7e6cd9ddb8ab9c4ecbe8ed9ad61d9138
ThinkingDobby <chiho1221@naver.com>
2021-11-01 23:00:20 +0900

    앱 최초 실행 시 기본 데이터 저장 기초 구현 완료
    <FIN>
    SharedPreference 이용해 앱 최초 실행 구분
    최초 실행 시 기본 데이터 저장하도록 구현(테스트)
    <TODO>
    이미지 회전 문제 고민해볼 것
    기본 데이터 저장 구현
    DetailActivity, update 구현

--- app/src/main/java/com/example/thinkingdobby/fishtracker/MenuActivity.kt
+++ app/src/main/java/com/example/thinkingdobby/fishtracker/MenuActivity.kt
@@ -1,12 +1,22 @@
 package com.example.thinkingdobby.fishtracker
 
 import android.content.Intent
-import android.support.v7.app.AppCompatActivity
+import android.graphics.Bitmap
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
+import android.support.v7.app.AppCompatActivity
 import android.view.View
+import android.widget.Toast
+import com.example.thinkingdobby.fishtracker.data.Fish
+import com.example.thinkingdobby.fishtracker.data.FishDB
 import kotlinx.android.synthetic.main.activity_menu.*
+import java.io.ByteArrayOutputStream
+import kotlin.concurrent.thread
+
 
 class MenuActivity : AppCompatActivity() {
+    private var fishDB: FishDB? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,5 +32,39 @@ class MenuActivity : AppCompatActivity() {
         menu_btn_back.setOnClickListener {
             finish()
         }
+
+        val pref = getSharedPreferences("isFirst", MODE_PRIVATE)
+        val first = pref.getBoolean("isFirst", false)
+        if (!first) {
+            val editor = pref.edit()
+            editor.putBoolean("isFirst", true)
+            editor.apply()
+            Toast.makeText(this@MenuActivity, "Test", Toast.LENGTH_SHORT).show()
+            thread {
+                fishDB = FishDB.getInstance(this@MenuActivity)
+                val fish = Fish()
+                fish.date = "11월 1일"
+                fish.fishName = "꺽지"
+                fish.fishNo = 3
+                fish.id = 123
+                fish.image = getByteArrayFromDrawable(Uri.parse("android.resource://com.example.thinkingdobby.fishtracker/drawable/collection_icon_carp"))
+                fishDB?.fishDao()?.insert(fish)
+            }
+        } else {
+            /*
+            val editor = pref.edit()
+            editor.putBoolean("isFirst", false)
+            editor.apply()
+
+             */
+        }
+    }
+
+    private fun getByteArrayFromDrawable(uri: Uri): ByteArray {
+        val bitmap = MediaStore.Images.Media.getBitmap(contentResolver, uri)
+        val stream = ByteArrayOutputStream()
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 10, stream)
+
+        return stream.toByteArray()
     }
 }
