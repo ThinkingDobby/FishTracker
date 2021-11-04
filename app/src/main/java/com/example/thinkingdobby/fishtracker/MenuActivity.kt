5ecda5474de4c5b6b47024054b9954e495faeaba
ThinkingDobby <chiho1221@naver.com>
2021-11-04 10:14:56 +0900

    기본 데이터 추가 구현
    <FIN>
    MenuActivity에서 기본 데이터 추가되도록(최초 실행 시) 구현
    디자인 세부 수정
    RecyclerView Layout Manager를 GridLayoutManager로 생성 시 격자 모양으로 칸이 생성됨
    - 안에 들어갈 요소 크기를 matchparent 나 wrapcontent로 설정 시 칸 안에 들어가게됨
    - 크기를 직접 지정하면 정렬이 작동하지 않음
    <TODO>
    DetailActivity 구현
    추후에 데이터 저장 부분 캡슐화

--- app/src/main/java/com/example/thinkingdobby/fishtracker/MenuActivity.kt
+++ app/src/main/java/com/example/thinkingdobby/fishtracker/MenuActivity.kt
@@ -43,6 +43,7 @@ class MenuActivity : AppCompatActivity() {
             editor.putBoolean("isFirst", true)
             editor.apply()
             Toast.makeText(this@MenuActivity, "Test", Toast.LENGTH_SHORT).show()
+            // 기본 데이터 추가
             thread {    // 비동기 처리 필요
                 // 테스트
                 fishDB = FishDB.getInstance(this@MenuActivity)
@@ -53,10 +54,26 @@ class MenuActivity : AppCompatActivity() {
                 val orientation = ei.getAttributeInt(ExifInterface.TAG_ORIENTATION,
                         ExifInterface.ORIENTATION_UNDEFINED)
 
+                val bass = Fish()
+                bass.date = "아직 수집되지 않았음"
+                bass.fishName = "배스"
+                bass.id = 1
+                bass.imgOt = orientation
+                bass.image = getByteArrayFromDrawable(uri)
+                fishDB?.fishDao()?.insert(bass)
+
+                val blue = Fish()
+                blue.date = "아직 수집되지 않았음"
+                blue.fishName = "블루길"
+                blue.id = 2
+                blue.imgOt = orientation
+                blue.image = getByteArrayFromDrawable(uri)
+                fishDB?.fishDao()?.insert(blue)
+
                 val carrasius = Fish()
                 carrasius.date = "아직 수집되지 않았음"
                 carrasius.fishName = "붕어"
-                carrasius.id = 1
+                carrasius.id = 3
                 carrasius.imgOt = orientation
                 carrasius.image = getByteArrayFromDrawable(uri)
                 fishDB?.fishDao()?.insert(carrasius)
@@ -64,10 +81,146 @@ class MenuActivity : AppCompatActivity() {
                 val carp = Fish()
                 carp.date = "아직 수집되지 않았음"
                 carp.fishName = "잉어"
-                carp.id = 2
+                carp.id = 4
                 carp.imgOt = orientation
                 carp.image = getByteArrayFromDrawable(uri)
                 fishDB?.fishDao()?.insert(carp)
+
+                val catfish = Fish()
+                catfish.date = "아직 수집되지 않았음"
+                catfish.fishName = "메기"
+                catfish.id = 5
+                catfish.imgOt = orientation
+                catfish.image = getByteArrayFromDrawable(uri)
+                fishDB?.fishDao()?.insert(catfish)
+
+                val channaArgus = Fish()
+                channaArgus.date = "아직 수집되지 않았음"
+                channaArgus.fishName = "가물치"
+                channaArgus.id = 6
+                channaArgus.imgOt = orientation
+                channaArgus.image = getByteArrayFromDrawable(uri)
+                fishDB?.fishDao()?.insert(channaArgus)
+
+                val chineseMinnow = Fish()
+                chineseMinnow.date = "아직 수집되지 않았음"
+                chineseMinnow.fishName = "버들치"
+                chineseMinnow.id = 7
+                chineseMinnow.imgOt = orientation
+                chineseMinnow.image = getByteArrayFromDrawable(uri)
+                fishDB?.fishDao()?.insert(chineseMinnow)
+
+                val chub = Fish()
+                chub.date = "아직 수집되지 않았음"
+                chub.fishName = "끄리"
+                chub.id = 8
+                chub.imgOt = orientation
+                chub.image = getByteArrayFromDrawable(uri)
+                fishDB?.fishDao()?.insert(chub)
+
+                val coreoperca = Fish()
+                coreoperca.date = "아직 수집되지 않았음"
+                coreoperca.fishName = "꺽지"
+                coreoperca.id = 9
+                coreoperca.imgOt = orientation
+                coreoperca.image = getByteArrayFromDrawable(uri)
+                fishDB?.fishDao()?.insert(coreoperca)
+
+                val darkChub = Fish()
+                darkChub.date = "아직 수집되지 않았음"
+                darkChub.fishName = "갈겨니"
+                darkChub.id = 10
+                darkChub.imgOt = orientation
+                darkChub.image = getByteArrayFromDrawable(uri)
+                fishDB?.fishDao()?.insert(darkChub)
+
+                val eel = Fish()
+                eel.date = "아직 수집되지 않았음"
+                eel.fishName = "장어"
+                eel.id = 11
+                eel.imgOt = orientation
+                eel.image = getByteArrayFromDrawable(uri)
+                fishDB?.fishDao()?.insert(eel)
+
+                val goby = Fish()
+                goby.date = "아직 수집되지 않았음"
+                goby.fishName = "모래무지"
+                goby.id = 12
+                goby.imgOt = orientation
+                goby.image = getByteArrayFromDrawable(uri)
+                fishDB?.fishDao()?.insert(goby)
+
+                val leather = Fish()
+                leather.date = "아직 수집되지 않았음"
+                leather.fishName = "향어"
+                leather.id = 13
+                leather.imgOt = orientation
+                leather.image = getByteArrayFromDrawable(uri)
+                fishDB?.fishDao()?.insert(leather)
+
+                val mandarinFish = Fish()
+                mandarinFish.date = "아직 수집되지 않았음"
+                mandarinFish.fishName = "쏘가리"
+                mandarinFish.id = 14
+                mandarinFish.imgOt = orientation
+                mandarinFish.image = getByteArrayFromDrawable(uri)
+                fishDB?.fishDao()?.insert(mandarinFish)
+
+                val masou = Fish()
+                masou.date = "아직 수집되지 않았음"
+                masou.fishName = "산천어"
+                masou.id = 15
+                masou.imgOt = orientation
+                masou.image = getByteArrayFromDrawable(uri)
+                fishDB?.fishDao()?.insert(masou)
+
+                val minnow = Fish()
+                minnow.date = "아직 수집되지 않았음"
+                minnow.fishName = "피라미"
+                minnow.id = 16
+                minnow.imgOt = orientation
+                minnow.image = getByteArrayFromDrawable(uri)
+                fishDB?.fishDao()?.insert(minnow)
+
+                val rainbow = Fish()
+                rainbow.date = "아직 수집되지 않았음"
+                rainbow.fishName = "무지개 송어"
+                rainbow.id = 17
+                rainbow.imgOt = orientation
+                rainbow.image = getByteArrayFromDrawable(uri)
+                fishDB?.fishDao()?.insert(rainbow)
+
+                val skin = Fish()
+                skin.date = "아직 수집되지 않았음"
+                skin.fishName = "누치"
+                skin.id = 18
+                skin.imgOt = orientation
+                skin.image = getByteArrayFromDrawable(uri)
+                fishDB?.fishDao()?.insert(skin)
+
+                val skygager = Fish()
+                skygager.date = "아직 수집되지 않았음"
+                skygager.fishName = "강준치"
+                skygager.id = 19
+                skygager.imgOt = orientation
+                skygager.image = getByteArrayFromDrawable(uri)
+                fishDB?.fishDao()?.insert(skygager)
+
+                val striped = Fish()
+                striped.date = "아직 수집되지 않았음"
+                striped.fishName = "돌고기"
+                striped.id = 20
+                striped.imgOt = orientation
+                striped.image = getByteArrayFromDrawable(uri)
+                fishDB?.fishDao()?.insert(striped)
+
+                val sweet = Fish()
+                sweet.date = "아직 수집되지 않았음"
+                sweet.fishName = "은어"
+                sweet.id = 21
+                sweet.imgOt = orientation
+                sweet.image = getByteArrayFromDrawable(uri)
+                fishDB?.fishDao()?.insert(sweet)
             }
         } else {
             // 테스트 위함 - 최초 실행 환경으로 전환
