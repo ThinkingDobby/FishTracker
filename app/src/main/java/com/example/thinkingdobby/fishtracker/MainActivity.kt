83b893414e5ed0f73c21068bde6aaada2e0d6580
ThinkingDobby <chiho1221@naver.com>
2022-02-02 00:02:26 +0900

    Dexter 권한 거부 시 다일로그 구현
    <FIN>
    거부된 권한 있을 시 다일로그 나타나도록 구현
    - 환경설정 페이지로 이동 가능하도록

--- app/src/main/java/com/example/thinkingdobby/fishtracker/MainActivity.kt
+++ app/src/main/java/com/example/thinkingdobby/fishtracker/MainActivity.kt
@@ -1,17 +1,20 @@
 package com.example.thinkingdobby.fishtracker
 
 import android.Manifest
+import android.app.AlertDialog
 import android.content.Intent
-import android.support.v7.app.AppCompatActivity
+import android.net.Uri
 import android.os.Bundle
 import android.os.Handler
+import android.provider.Settings
+import android.support.v7.app.AppCompatActivity
 import android.view.View
-import android.widget.Toast
 import com.karumi.dexter.Dexter
 import com.karumi.dexter.MultiplePermissionsReport
 import com.karumi.dexter.PermissionToken
 import com.karumi.dexter.listener.PermissionRequest
 import com.karumi.dexter.listener.multi.MultiplePermissionsListener
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -30,22 +33,43 @@ class MainActivity : AppCompatActivity() {
                             if (report.areAllPermissionsGranted()) {
                                 startLoading()
                             } else {
-                                Toast.makeText(this@MainActivity, "앱을 재실행한 후 권한을 허용해주세요.", Toast.LENGTH_SHORT).show()   // 임시
+                                showSettingsDialog()
                             }
                         }
                     }
 
-                    override fun onPermissionRationaleShouldBeShown(permissions: List<PermissionRequest>, token: PermissionToken) { /* ... */
+                    override fun onPermissionRationaleShouldBeShown(permissions: List<PermissionRequest>, token: PermissionToken) {
+                        showSettingsDialog()
                     }
                 }).check()
 
         window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
     }
 
+    private fun showSettingsDialog() {
+        val builder = AlertDialog.Builder(this)
+        builder.setMessage(R.string.need_permission)
+        builder.setPositiveButton("설정으로 이동") { dialog, _ ->
+            dialog.cancel()
+            openSettings()
+        }
+        builder.setNegativeButton("취소") { dialog, _ -> dialog.cancel() }
+        builder.show()
+    }
+
+
+    // 어플리케이션 정보 설정 페이지
+    private fun openSettings() {
+        val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+        val uri: Uri = Uri.fromParts("package", packageName, null)
+        intent.data = uri
+        startActivityForResult(intent, 101)
+    }
+
     private fun startLoading() {
         val handler = Handler()
         handler.postDelayed({
-            val intent  = Intent(this, TrackerActivity::class.java)
+            val intent = Intent(this, TrackerActivity::class.java)
             finish()
             startActivity(intent)
         }, 700)
